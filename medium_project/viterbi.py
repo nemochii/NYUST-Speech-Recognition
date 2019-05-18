efa34880f06eca6834b573b0bc0310b66e9378aa
Jerry <jerry90127@gmail.com>
2019-05-18 12:39:34 +0800

    add HMM_discrete

---
+++ medium_project/viterbi.py
@@ -0,0 +1,43 @@
+import numpy as np
+from dspBox import str2ndar
+
+def run(allopt, eachopt, pi):
+	with open("check\\test.txt") as f:
+		lines = f.readlines()
+		for content in lines:
+			data = str2ndar(content)
+
+			forward_S = np.zeros([3, 3])
+			temp = np.zeros([3, 3])
+			max_state = np.zeros([3])
+			back_path = np.zeros([3, len(data), 3])
+			path = np.zeros([3, len(data)], dtype = np.int8)
+			for i in range(len(data)):
+				word = data[i]
+
+				if i == 0:
+					for model in range(3):
+						for state in range(3):
+							forward_S[model, state] = pi[model, state] * eachopt[model, state, word]
+					continue
+
+				for model in range(3):
+					for state in range(3):
+						for weight in range(3):
+							max_state[weight] = allopt[model, weight, state] * forward_S[model, weight]
+						temp[model, state] = np.max(max_state) * eachopt[model, state, word]
+						back_path[model, i, state] = np.argmax(max_state)
+				
+				for model in range(3):
+					for state in range(3):
+						forward_S[model, state] = temp[model, state]
+				
+				if i == len(data) - 1:
+					for model in range(3):
+						path[model, i] = np.argmax(forward_S, axis = 1)[model]
+
+						for k in range(i - 1, -1, -1):
+							path[model, k] = back_path[model, k + 1, path[model, k + 1]]
+			forward_S = np.max(forward_S, axis = 1)
+			
+			print ("model %d" % (np.argmax(forward_S) + 1))
